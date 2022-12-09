db54210188c64c5b20f2a565164f0ece987ad3d0
christiantsg <kanemarinp2002@gmail.com>
2022-12-08 22:23:04 -0800

    checkpoint pt2
    checkpoint pt2

--- filtercard.js
+++ filtercard.js
@@ -9,12 +9,14 @@ function renderParksCards(parks) {
     let park = parks[i]
   
         const cardTemplate = `<div class="card" style="width: 18rem;">
-            <img class="card-img" src="/images/Bond-StoryImage_3.jpg">
-            <div class="card-body">
-                <h5 class="card-title">${park.LocationName}</h5>
-                <p class="card-descrip">${park.Address}</p>
-            </div>
-        </div>`
+        <img class="card-img-top" src="/images/Adams-StoryImage_2.jpg"> 
+        <div class="card-body">
+          <h5 class="card-title">${park.LocationName}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`
+      
          //innerHTML
        cardsMain.insertAdjacentHTML("beforeend", cardTemplate)
     }
